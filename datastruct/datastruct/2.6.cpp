eadde6cf65ce478b2717f46d3c9ecf3835345e23
SilkSilkSilk <2461220952@qq.com>
2018-01-21 18:22:53 +0800

    广义表

---
+++ datastruct/datastruct/2.6.cpp
@@ -0,0 +1,114 @@
+#include "2.6.h"
+
+
+namespace Main_2_6
+{
+	Glist list_MakeEmpty(const ElementType index) //建立广义单元
+	{
+		Glist ptrl = new GNode();
+
+		ptrl->tag = 1;
+		ptrl->URegion.SybList = nullptr;
+		ptrl->index = index;
+		ptrl->Next = nullptr;
+
+		return ptrl;
+	}
+
+
+	//系数和指数
+	Glist indi_MakeEmpty(const ElementType coef, const ElementType index) //建立个体单元
+	{
+		Glist ptrl = new GNode();
+
+		ptrl->tag = 0;
+		ptrl->URegion.Data = coef;
+		ptrl->index = index;
+		ptrl->Next = nullptr;
+
+		return ptrl;
+	}
+
+
+	Glist add_member_right(Glist ptrli, Glist ptr) //列表向右添加元素
+	{
+		if (ptr == nullptr)
+		{
+			return nullptr;
+		}
+		ptrli->Next = ptr;
+
+		return ptr;
+	}
+
+
+	Glist add_member_down(Glist ptrli, Glist ptr) //广域表向下添加元素
+	{
+		if (ptr == nullptr || ptrli->tag == 0) //非广域表
+		{
+			return nullptr;
+		}
+		ptrli->URegion.SybList = ptr;
+
+		return ptr;
+	}
+
+
+	void print(Glist ptrl)			//打印
+	{
+		Glist ptr = ptrl;
+
+		if (ptr == nullptr)
+		{
+			return;
+		}
+
+		while (ptr != nullptr)
+		{
+			if (ptr->tag == 1)
+			{
+				auto ptr1 = ptr->URegion.SybList;
+				cout << "(";
+
+				for (int i = 0; ptr1 != nullptr; ptr1 = ptr1->Next)
+				{
+					if (i != 0)
+					{
+						cout << ( ptr1->URegion.Data > 0 ? " + " : " - " );
+					}
+					cout << abs(ptr1->URegion.Data) << "y" << ptr1->index;
+					i++;
+				}
+
+				cout << ")x" << ptr->index << " + ";
+			}
+			else
+			{
+				cout << ptr->URegion.Data << "x" << ptr->index;
+			}
+			ptr = ptr->Next;
+		}
+		cout << endl;
+	}
+
+
+	int main_2_6()
+	{
+		Glist P1 = list_MakeEmpty(12);
+		Glist P2 = list_MakeEmpty(8);
+		Glist P3 = indi_MakeEmpty(3, 2);
+
+		Glist list = add_member_down(P1, indi_MakeEmpty(9, 2));
+		add_member_right(list, indi_MakeEmpty(4, 0));
+
+		list = add_member_down(P2, indi_MakeEmpty(15, 3));
+		add_member_right(list, indi_MakeEmpty(-1, 1));
+
+		list = add_member_right(P1, P2);
+		list = add_member_right(list, P3);
+
+		print(P1);
+
+		return 0;
+	}
+}
--- datastruct/datastruct/main.cpp
+++ datastruct/datastruct/main.cpp
@@ -3,6 +3,7 @@
 
 #include "2.4.h"
 #include "2.2.h"
+#include "2.6.h"
 
 
 using namespace std;
@@ -11,6 +12,8 @@ int main()
 {
 //	Main_2_2::main_2_2();
 //	Main_2_4::main_2_4();
+
+	Main_2_6::main_2_6();
 
 	return 0;
 }
