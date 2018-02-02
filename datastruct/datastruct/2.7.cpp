bd29a329a92b6e1669cd06b561aabfe7965e5655
SilkSilkSilk <2461220952@qq.com>
2018-02-02 21:08:51 +0800

    多重链表矩阵存储的深入理解，思路已经清晰，明天实现

--- datastruct/datastruct/2.7.cpp
+++ datastruct/datastruct/2.7.cpp
@@ -2,14 +2,58 @@
 
 
 namespace Main_2_7
-{//多重链表还是没有去实现，因为感觉太过繁琐，还有准备明天从学校回家
+{//多重链表矩阵存储的深入理解，思路已经清晰
+
+	shared_ptr<MNode[]> term;
+
 	MList Make_init(const vector<vector<int>> &list)
 	{
 		int m = list.size(), n = list[0].size();
 
 		cout << m << " " << n << endl;
 
-		return MList();
+		int nod = m > n ? m : n;		//取最大行列的最大数
+
+		if (nod >= Max_head)
+		{
+			return nullptr;		//表错误
+		}
+
+//		MList term = new MNode[nod + 1]();
+		shared_ptr<MNode[]> tm(new MNode[nod + 1]());
+		term = tm;
+//		shared_ptr<MNode[]> term = make_shared<MNode[3 + 1]>();  //这种方式不能用nod变量
+
+		term[0].tag = 1;
+		term[0].Urcvn.node.Row = m;		//行和列
+		term[0].Urcvn.node.Col = n;
+
+		for (int i = 1; i <= nod; i++)
+		{
+			if(i == 1)
+			{
+				term[0].Right = &term[i];
+			}
+			else
+			{
+				term[i - 1].Urcvn.Next = &term[i];
+			}
+			term[i].Down = &term[i];
+			term[i].Right = &term[i];
+		}
+
+		term[nod].Urcvn.Next = &term[0];
+		
+		for (int i = 0; i < m; i++)			//遍历vector
+		{
+			for (int j = 0; j < n; j++)
+			{
+
+			}
+		}
+
+
+		return &term[0];
 	}
 
 
@@ -23,6 +67,8 @@ namespace Main_2_7
 		};
 
 		Make_init(a);
+
+		
 
 		return 0;
 	}
--- datastruct/datastruct/2.7.h
+++ datastruct/datastruct/2.7.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>
+
 
 namespace Main_2_7
 {//多重链表的矩阵实例运用
@@ -13,23 +15,27 @@ namespace Main_2_7
 	typedef struct MNode *MList;
 	struct MNode
 	{
-		int tag;		//区分term和head类型，非0行列节点表示term
+		int tag;		//区分term和head类型，非0行列节点表示term, term = 1 
 		MList Down;		//接受列指针，上下
 
 		union
 		{
-			struct node		//节点数据
+			struct	//节点数据
 			{
 				int Row;	//行和列
 				int Col;	
 				int Value;	//数据
-			};
+			}node;
 
 			MList Next;
 		}Urcvn;
 		
 		MList Right;		//接受右
 	};
+
+	#define Max_head 1024
+
+	extern shared_ptr<MNode[]> term;
 
 	MList Make_init(const vector<vector<int>> &list);
 
