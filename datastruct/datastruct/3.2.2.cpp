a5fe9c0a6224b78a90ed3d28bf19dadc01ad15d8
SilkSilkSilk <2461220952@qq.com>
2018-05-24 21:00:59 +0800

    二叉树

--- datastruct/datastruct/3.2.2.cpp
+++ datastruct/datastruct/3.2.2.cpp
@@ -1,11 +1,23 @@
 ﻿#include "3.2.2.h"
 
 namespace Main_3_2_2
-{	//数组实现二叉树存储
+{	//链表实现二叉树存储
 
 	void main_3_2_2()
 	{
-		class_3_2_2<int> a(100);			//建立空的二叉树
+		const vector<int> s = { 1, 2, 4, 8, null_Bin, null_Bin, null_Bin, null_Bin, 3, 6, 12, null_Bin, null_Bin, null_Bin, 7, null_Bin, 15, null_Bin, null_Bin };
+
+		class_3_2_2<int> a(s, 100);			//建立空的二叉树
+
+		for(auto i : s)
+		{
+			cout << i << ' ';
+		}
+		cout << endl;
+
+		cout << "前序遍历：" << endl;
+		
+		a.PreOrderTraversal(a.local_tree);
 	}
 
 }
--- datastruct/datastruct/3.2.2.h
+++ datastruct/datastruct/3.2.2.h
@@ -11,12 +11,12 @@ namespace Main_3_2_2
 {	//链表实现二叉树存储
 	using namespace std;
 
-
 	template<class T> class class_3_2_2
 	{
 	public:
 #define ERROR -1010
 		typedef T ElementType;
+#define null_Bin 110011							//空节点
 
 		struct TreeNode
 		{
@@ -26,22 +26,50 @@ namespace Main_3_2_2
 		};
 
 		typedef shared_ptr<TreeNode> BinTree;					//智能指针管理内存
-		BinTree local_tree;
-		int Maxsize;
+		BinTree local_tree = nullptr;
+		int BinTree_size = -1;
+		int BinTree_Maxsize;
 
-		void BinTree_Creat()			
+		void BinTree_Creat()
 		{
-			local_tree = make_shared<TreeNode>();				//初始化智能指针 TreeNode默认构造
+			local_tree = make_shared<TreeNode>();
 
-//			local_tree->Left = nullptr;							//这里不需要再次nullptr操作，因为TreeNode会执行默认初始化，指针默认初始化0，int默认初始化0
-//			local_tree->Right = nullptr;
-//			local_tree->Data = 0;
+			local_tree->Left = nullptr;
+			local_tree->Right = nullptr;
+			local_tree->Data = 0;
 		}
 
-		class_3_2_2(const int max_size = 1024)			//构造	初始化队列认	默认长度为1024
+		//先序创建二叉树
+		BinTree BinTree_Creat(const vector<int> &s)
 		{
-			BinTree_Creat();
-			Maxsize = max_size;
+			++BinTree_size;
+			BinTree bt;
+
+			if (BinTree_size >= BinTree_Maxsize)
+			{
+				return nullptr;
+			}
+			else
+			{
+				if (s[BinTree_size] == null_Bin)
+				{
+					bt = nullptr;
+				}
+				else
+				{
+					bt = make_shared<TreeNode>();
+					bt->Data = s[BinTree_size];
+					bt->Left = BinTree_Creat(s);
+					bt->Right = BinTree_Creat(s);
+				}
+			}
+			return bt;
+		}
+
+		class_3_2_2(const vector<int> &s, const int max_size = 1024) : BinTree_size(-1)
+		{
+			BinTree_Maxsize = max_size;
+			local_tree = BinTree_Creat(s);  //最后返回根
 		}
 
 		~class_3_2_2()
@@ -49,6 +77,15 @@ namespace Main_3_2_2
 			//析构
 		}
 
+		void PreOrderTraversal(BinTree bt)
+		{
+			if (bt != nullptr)
+			{
+				cout << bt->Data << ' ';
+				PreOrderTraversal(bt->Left);
+				PreOrderTraversal(bt->Right);
+			}
+		}
 
 	};
 
