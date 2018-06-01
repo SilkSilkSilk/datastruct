f3e206b383fde3627923e58382de9529a9d59dad
SilkSilkSilk <2461220952@qq.com>
2018-06-01 20:19:59 +0800

    实现了前序和中序的构建
    只是可以实现而已，代码很粗陋，有vector的分配和数据考呗，还有将左右子树的存储使用原vector的下标方法实现会好很多。

--- datastruct/datastruct/3.2.2.cpp
+++ datastruct/datastruct/3.2.2.cpp
@@ -1,15 +1,15 @@
 ﻿#include "3.2.2.h"
 
 namespace Main_3_2_2
-{	//链表实现二叉树存储
+{ //链表实现二叉树存储
 
 	void main_3_2_2()
 	{
-		const vector<int> s = { 1, 2, 4, 8, null_Bin, null_Bin, null_Bin, null_Bin, 3, 6, 12, null_Bin, null_Bin, null_Bin, 7, null_Bin, 15, null_Bin, null_Bin };
+		const vector<int> s = {1, 2, 4, 8, null_Bin, null_Bin, null_Bin, null_Bin, 3, 6, 12, null_Bin, null_Bin, null_Bin, 7, null_Bin, 15, null_Bin, null_Bin};
 
-		class_3_2_2<int> a(s, 100);			//建立二叉树
+		class_3_2_2<int> a(s, 100); //建立二叉树
 
-		for(auto i : s)
+		for (auto i : s)
 		{
 			cout << i << ' ';
 		}
@@ -28,7 +28,7 @@ namespace Main_3_2_2
 
 		cout << endl << "堆栈中序：" << endl;
 		a.InOrderTraversal_stack(a.local_tree);
-		
+
 		cout << endl << "堆栈后序：" << endl;
 		a.PostOrderTraversal_stack(a.local_tree);
 
@@ -36,6 +36,20 @@ namespace Main_3_2_2
 		a.LevelOrderTraversal(a.local_tree);
 
 		cout << endl << "树的高度：" << a.PostOrderGetHeight(a.local_tree) << endl;
-	}
 
+		vector<int> s1 = {1, 2, 4, 8, 5, 3, 7};
+		vector<int> s2 = {8, 4, 2, 5, 1, 3, 7};
+		class_3_2_2<int> a1(s1, s2, 100); //建立二叉树
+
+		cout << endl << "a1 堆栈前序：" << endl;
+		a1.PreOrderTraversal_stack(a1.local_tree);
+
+
+		vector<int> s3 = { 1, 2, 4, 8, 3, 6, 12, 7, 15 };
+		vector<int> s4 = { 8, 4, 2, 1, 12, 6, 3, 7, 15 };
+		class_3_2_2<int> a2(s3, s4, 100); //建立二叉树
+
+		cout << endl << "a2 堆栈前序：" << endl;
+		a2.PreOrderTraversal_stack(a2.local_tree);
+	}
 }
--- datastruct/datastruct/3.2.2.h
+++ datastruct/datastruct/3.2.2.h
@@ -68,10 +68,72 @@ namespace Main_3_2_2
 			return bt;
 		}
 
-		class_3_2_2(const vector<int> &s, const int max_size = 1024) : BinTree_size(-1)
+		//先序和中序创建二叉树
+		BinTree BinTree_Creat(vector<int> &s1, vector<int> &s2)
+		{
+			if (s1.empty() || s2.empty())
+			{
+				return nullptr;
+			}
+
+			BinTree bt;
+			vector<int> s1_l, s1_r;
+			vector<int> s2_l, s2_r;
+
+			vector<int>::iterator it = find(s2.begin(), s2.end(), s1[0]);	//在中序数组中找左右子树
+			if (it != s2.end())	//找不到应该是错误
+			{
+				s2_l.insert(s2_l.begin(), s2.begin(), it);		//左子树
+				s2_r.insert(s2_r.begin(), it + 1, s2.end());		//右子树
+			}
+
+			//if (!s2_r.empty())	//右子树不为空
+			//{
+			//	it = find(s1.begin() + 1, s1.end(), s2_r[0]);	//在前序中区分左右子树 根据“根”的右边的元素
+			//	if (it != s1.end())	//找不到应该是错误
+			//	{
+			//		s1_l.insert(s1_l.begin(), s1.begin() + 1, it);		//左子树
+			//		s1_r.insert(s1_r.begin(), it, s1.end());			//右子树
+			//	}
+			//}
+			//else if (!s2_l.empty())	//当右子树为空时，使用左子树
+			//{
+			//	it = find(s1.begin() + 1, s1.end(), s2_l[0]);	//在前序中区分左右子树 根据“根”的左边的元素
+			//	if (it != s1.end())	//找不到应该是错误
+			//	{
+			//		s1_l.insert(s1_l.begin(), s1.begin() + 1, it + 1);		//左子树
+			//		s1_r.insert(s1_r.begin(), it + 1, s1.end());			//右子树
+			//	}
+			//}
+			if (!s2_l.empty())	//左子树不为空
+			{
+				s1_l.insert(s1_l.begin(), s1.begin() + 1, s1.begin() + 1 + s2_l.size());		//左子树
+				s1_r.insert(s1_r.begin(), s1.begin() + 1 + s2_l.size(), s1.end());			//右子树
+			}
+			else
+			{
+				s1_r.insert(s1_r.begin(), s1.begin() + 1, s1.end());			//右子树
+			}
+			
+			
+			bt = make_shared<TreeNode>();
+			bt->Data = s1[0];
+			bt->Left = BinTree_Creat(s1_l, s2_l);
+			bt->Right = BinTree_Creat(s1_r, s2_r);
+
+			return bt;
+		}
+
+		class_3_2_2(const vector<int> &s, const int max_size = 1024) : BinTree_size(-1)	//先序创建二叉树
 		{
 			BinTree_Maxsize = max_size;
 			local_tree = BinTree_Creat(s);  //最后返回根
+		}
+
+		class_3_2_2(vector<int> &s1, vector<int> &s2, const int max_size = 1024) : BinTree_size(-1)	//先序和中序创建二叉树
+		{
+			BinTree_Maxsize = max_size;
+			local_tree = BinTree_Creat(s1, s2);  //最后返回根
 		}
 
 		~class_3_2_2()
