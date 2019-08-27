dd97238472e80552b679b5082b64986971c43381
SilkSilkSilk <2461220952@qq.com>
2019-08-27 22:07:49 +0800

    heap delete function

--- datastruct/datastruct/5.1.cpp
+++ datastruct/datastruct/5.1.cpp
@@ -6,7 +6,52 @@ namespace Main_5_1
 	void main_5_1()
 	{
 		class_5_1<int> a(10);
+		a.Insert(1);
+		a.Insert(2);
+		a.Insert(3);
+		a.Insert(4);
+		a.Insert(5);
+		a.Insert(6);
+		a.Insert(7);
+		a.Insert(3);
+		a.print();
 
+		bool is_success = false;
+		int ret = 0;
 
+		ret = a.DeleteMax(&is_success);
+		if (is_success)
+		{
+			cout << "Throw the highest priority task : " << ret << endl;
+		}
+		a.print();
+
+		ret = a.DeleteMax(&is_success);
+		if (is_success)
+		{
+			cout << "Throw the highest priority task : " << ret << endl;
+		}
+		a.print();
+
+		ret = a.DeleteMax(&is_success);
+		if (is_success)
+		{
+			cout << "Throw the highest priority task : " << ret << endl;
+		}
+		a.print();
+
+		ret = a.DeleteMax(&is_success);
+		if (is_success)
+		{
+			cout << "Throw the highest priority task : " << ret << endl;
+		}
+		a.print();
+
+		ret = a.DeleteMax(&is_success);
+		if (is_success)
+		{
+			cout << "Throw the highest priority task : " << ret << endl;
+		}
+		a.print();
 	}
 }
--- datastruct/datastruct/5.1.h
+++ datastruct/datastruct/5.1.h
@@ -22,6 +22,7 @@ namespace Main_5_1
 	template<class T> class class_5_1
 	{
 	public:
+		#define MaxData 1000	//max priority
 		typedef T ElementType;
 		typedef shared_ptr<ElementType> Elementptr;
 		struct HeapStruct
@@ -36,7 +37,11 @@ namespace Main_5_1
 		class_5_1(int maxsize);
 		~class_5_1();
 		Heap Creat(int maxsize);
-
+		bool IsFull();
+		bool IsEmpty();
+		void print();
+		void Insert(ElementType item);
+		ElementType DeleteMax(bool* is_success);
 	};
 
 	template<class T>
@@ -51,15 +56,125 @@ namespace Main_5_1
 	}
 
 	template<class T>
-	inline class_5_1<T>::Heap class_5_1<T>::Creat(int maxsize)
+	inline typename class_5_1<T>::Heap class_5_1<T>::Creat(int maxsize)
 	{
 		Heap heap = make_shared<HeapStruct>();
-		heap->elements.resize(maxsize);
-		heap->size = 0;
-		heap->Capacity = maxsize;
+		heap->elements.push_back(make_shared<ElementType>(MaxData));
 
+		for (auto i = 0; i < maxsize; i++)
+		{
+			heap->elements.push_back(make_shared<ElementType>(0));
+		}
+		
+		heap->Size = 0;
+		heap->Capacity = maxsize;
 		return heap;
 	}
+
+	template<class T>
+	inline bool class_5_1<T>::IsFull()
+	{
+		if (localheap->Size >= localheap->Capacity)
+			return true;
+		else
+			return false;
+	}
+
+	template<class T>
+	inline bool class_5_1<T>::IsEmpty()
+	{
+		if (localheap->Size <= 0)
+			return true;
+		else
+			return false;
+	}
+
+	template<class T>
+	inline void class_5_1<T>::print()		//≤„–Ú±È¿˙
+	{
+		if (IsEmpty())
+		{
+			cout << "heap is empty" << endl;
+			return;
+		}
+
+		int h_l = -1;
+		for (auto i = 1; i <= localheap->Size; i++)
+		{
+			if (localheap->elements[i] == nullptr)
+				break;
+
+			cout << *localheap->elements[i] << " ";
+
+			int h = static_cast<int>(log(i + 1) / log(2));
+			if (h != h_l)
+			{
+				cout << endl;
+				h_l = h;
+			}
+		}
+		cout << endl;
+	}
+
+	template<class T>
+	inline void class_5_1<T>::Insert(ElementType item)
+	{
+		if (IsFull())
+		{
+			cout << "heap is full" << endl;
+			return;
+		}
+
+		int i = ++localheap->Size;
+		for (; *localheap->elements[i / 2] < item; i /= 2)
+		{
+			localheap->elements[i] = localheap->elements[i / 2];
+		}
+		localheap->elements[i] = make_shared<ElementType>(item);
+	}
+
+	template<class T>
+	inline typename class_5_1<T>::ElementType class_5_1<T>::DeleteMax(bool* is_success)
+	{
+		if (IsEmpty())
+		{
+			if (is_success)
+			{
+				*is_success = false;
+			}
+			return ElementType();
+		}
+
+		int parent, child;
+		ElementType Maxitem;
+		Elementptr temp;
+
+		Maxitem = *localheap->elements[1];
+		*localheap->elements[1] = *localheap->elements[localheap->Size--];
+		for (parent = 1; parent * 2 <= localheap->Size; parent = child)
+		{
+			child = parent * 2;
+			if (child < localheap->Size && (*localheap->elements[child] < *localheap->elements[child + 1]))
+			{
+				child++;
+			}
+			
+			if (*localheap->elements[parent] < *localheap->elements[child])
+			{
+				temp = localheap->elements[parent];
+				localheap->elements[parent] = localheap->elements[child];
+				localheap->elements[child] = temp;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		*is_success = true;
+		return Maxitem;
+	}
+	
 
 	void main_5_1();
 }
