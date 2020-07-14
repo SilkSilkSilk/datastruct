3680fd013d3a3fdd10172f5aa0cb109c57b64657
SilkSilkSilk <2461220952@qq.com>
2020-07-14 21:18:19 +0800

    三数和值最接近tatget
    我只解决了最接近tatget的部分，三数之和没有分析出数学特征。

--- datastruct/datastruct/Leetcode.cpp
+++ datastruct/datastruct/Leetcode.cpp
@@ -8,56 +8,42 @@ public:
 	int threeSumClosest(vector<int>& nums, int target)
 	{
 		deque<int> dq;
+		int value = 0;
+		int v_abs_min = -1;
+		sort(nums.begin(), nums.end());
 		for (int i = 0; i < nums.size() - 2; i++)
 		{
 			if (i != 0 && nums[i] == nums[i - 1])
 				continue;
-			int k = nums.size() - 1;
-			for (int j = i + 1; j < nums.size(); j++)
+			for (int j = i + 1; j < nums.size() - 1; j++)
 			{
-				if (j != i + 1 && nums[j] == nums[j - 1])
+				if (j != i + 1 && (nums[j] == nums[j - 1]))
 					continue;
-				while (-(nums[i] + nums[j]) > nums[k])
+				// while (j < k && -(nums[i] + nums[j]) < nums[k])
+				// {
+				// 	k--;
+				// }
+				// if (k == j)
+				// 	break;
+				for (int k = nums.size() - 1; k > j; k--)
 				{
-					k--;
+					const int abc = nums[i] + nums[j] + nums[k];
+					if (v_abs_min == -1 || abs(abc - target) < v_abs_min)
+					{
+						v_abs_min = abs(abc - target);
+						value = abc;
+					}
 				}
-				if (k <= j)
-					break;
-				dq.push_back(nums[i] + nums[j] + nums[k]);
 			}
 		}
-		if (dq.size() == 0)
-			return 0;
-
-		Main_9_1::quick_shell_sort(&dq, dq.size()); //¿ìËÙ+shell 32ÅÅÐò
-		int left = 0, right = dq.size() - 1, mid;
-		while (left <= right)
-		{
-			mid = (left + right) / 2;
-			if (dq[mid] > target)
-			{
-				right = mid - 1;
-			}
-			else if (dq[mid] < target)
-			{
-				left = mid + 1;
-			}
-			else
-			{
-				return target;
-			}
-		}
-		if (left == dq.size())
-		{
-			return dq.back();
-		}
-		return dq[left];
+		return value;
 	}
 };
 
 void main_Leetcode()
 {
 	Solution solution;
-	vector<int> vr = {-1, 3, 5, 8, 0, 2, 1, -4};
+	vector<int> vr = { 0, 2, 1, -3 };
 	cout << solution.threeSumClosest(vr, 1) << endl;
 }
+
